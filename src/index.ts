fb9e259b809543b0431d8ae5c9e5e2d10af1da2f
hugo <hugo@enrolmy.com>
2022-12-23 10:25:24 +1300

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,71 @@
+import { Request, Response } from "express";
+import { Result, ValidationError, validationResult } from "express-validator";
+const httpMocks = require("node-mocks-http")
+import _ from "lodash";
+
+/** Creates a bearer authorization header */
+const bearerAuthHeader = (key: string): object => {
+    return { Authorization: `Bearer ${key}` };
+};
+
+/**
+ * Mock to run an express-validator middleware chain. Takes functionality out of the route itself
+ *
+ * Makes it easy to test validation middleware
+ *
+ * https://stackoverflow.com/questions/28769200/unit-testing-validation-with-express-validator
+ *
+ * @param req Request
+ * @param res Response
+ * @param validationChain
+ * @return Result containing validation errors
+ */
+async function testValidationChain(
+    req: Request,
+    res: Response,
+    validationChain: Array<any>
+): Promise<Result<ValidationError>> {
+    await Promise.all(
+        validationChain.map(async (middleware) => {
+            await middleware(req, res, () => undefined);
+        })
+    );
+    return validationResult(req);
+}
+
+/**
+ * Tests required values in a request body
+ *
+ * @param requiredValuePaths
+ * @param body
+ * @param validationChain
+ */
+async function testRequiredValues(
+    requiredValuePaths: Array<string>,
+    body: any,
+    validationChain: Array<any>
+) {
+    let request = httpMocks.createRequest({ body: body });
+    let response = httpMocks.createResponse();
+    expect(
+        (
+            await testValidationChain(request, response, validationChain)
+        ).isEmpty()
+    ).toBeTruthy();
+
+    request = httpMocks.createRequest({
+        body: _.omit(body, requiredValuePaths),
+    });
+    response = httpMocks.createResponse();
+    expect(
+        (
+            await testValidationChain(request, response, validationChain)
+        ).isEmpty()
+    ).toBeFalsy();
+}
+
+module.exports = {
+    bearerAuthHeader,
+    testValidationChain,
+    testRequiredValues,
+};
