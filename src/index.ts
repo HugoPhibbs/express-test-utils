1b1af89e85342c064d4bfd38b358003f42ecad6f
HugoPhibbs <hugo@enrolmy.com>
2023-01-09 13:03:50 +1300

    adding authentication util function.
    changing behaviour of checking for required values. Each path is checked individually

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import {ValidationError, Result} from "express-validator";
+import {Request, Response, NextFunction} from "express"
 
 const { validationResult } = require("express-validator")
 const httpMocks = require("node-mocks-http")
 const _ = require("lodash")
+
+const sinon = require("sinon")
 
 /** Creates a bearer authorization header */
 const bearerAuthHeader = (key: string): object => {
@@ -37,13 +40,13 @@ export async function validationErrors(
 /**
  * Tests required values in a request body
  *
- * @param requiredValuePaths
- * @param body
- * @param validationChain
+ * @param requiredValuePaths strings for the paths of required values within a body. Checks these are required one by one
+ * @param body object for an object to be added to an express request
+ * @param validationChain array for an express-validator validation chain to check against a request body
  */
 export async function testRequiredBodyValues(
     requiredValuePaths: Array<string>,
-    body: any,
+    body: object,
     validationChain: Array<any>
 ) {
     let request = httpMocks.createRequest({ body: body });
@@ -54,15 +57,17 @@ export async function testRequiredBodyValues(
         ).isEmpty()
     ).toBeTruthy();
 
-    request = httpMocks.createRequest({
-        body: _.omit(body, requiredValuePaths),
-    });
-    response = httpMocks.createResponse();
-    expect(
-        (
+    for (let path of requiredValuePaths) {
+        request = httpMocks.createRequest({
+            body: _.omit(body, path),
+        });
+        response = httpMocks.createResponse();
+        expect(
+          (
             await validationErrors(request, response, validationChain)
-        ).isEmpty()
-    ).toBeFalsy();
+          ).isEmpty()
+        ).toBeFalsy();
+    }
 }
 
 /**
@@ -76,9 +81,22 @@ async function checkForValidationErrors(request: Request, validationChain: Array
     expect((await validationErrors(request, httpMocks.createResponse(), validationChain)).isEmpty()).toBe(shouldBeNoErrors)
 }
 
+/**
+ * Checks that a request passes authentication with the given authenticate function
+ *
+ * @param request Request to be checked
+ * @param authenticate authentication function to run. Expected to mark a response with a 401 status code, otherwise a next() function should be called
+ */
+async function checkRequestAuthentication(request: Request, authenticate : (req:Request, res:Response, next: NextFunction)=>void) {
+    const nextSpy = sinon.spy()
+    const response = httpMocks.createResponse()
+    await authenticate(request, response, nextSpy)
+    expect(response.statusCode)
+}
+
 module.exports = {
     bearerAuthHeader,
-    testValidationChain: validationErrors,
+    validationErrors,
     testRequiredBodyValues,
     checkForValidationErrors
 };
